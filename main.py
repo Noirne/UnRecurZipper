bd0e32759b2c723c8e214aeb4837f605d805ad24
Richez Antoine <richez.antoine.richez@gmail.com>
2019-07-05 00:52:06 +0200

    Currently working, the delete of zip files are still in WIP

--- main.py
+++ main.py
@@ -4,49 +4,47 @@ import glob
 
 # When an archive is found in a directory
 class ZipFile(object):
-    def __init__(self, path, name):
+    def __init__(self, path):
         self.path = path
-        self.name = name
         zip_ref = zipfile.ZipFile(self.path, 'r')
+        self.path = self.path.split('.zip')[0]
+        print("Current zip is at: " + self.path)
         self.UnzipFile(zip_ref)
     
     def UnzipFile(self, zip_ref):
-        zip_ref.extractall(self.name)
-        extractedFolder = File(os.path.join(self.path,self.name), list(), list())
-        os.remove(self.name + '.zip')
+        zip_ref.extractall(self.path)
+        Folder(self.path)
+        #os.remove(self.name + '.zip')
 
 # Actual directory that we could find somewhere
 class Folder:
-    def __init__(self, path, directoriesToCheck, filesToUnzip):
+    def __init__(self, path):
         self.path = path
-        self.directoriesToCheck = directoriesToCheck
-        self.filesToUnzip = filesToUnzip
+        print("Current working folder is: " + self.path)
+        self.checkForZippedFile()
+        self.checkForDirectories()
+        
 
     def checkForZippedFile(self):
-        self.filesToUnzip = glob.glob('*.zip', recursive=True)
+        self.filesToUnzip = list()
+        self.filesToUnzip = glob.glob(os.path.join(self.path,'*.zip'), recursive=True)
         for fileToUnzip in self.filesToUnzip:
-            fileToUnzip = ZipFile(os.path.join(self.path, fileToUnzip), fileToUnzip.split('.',1)[0])
-        return self.filesToUnzip
+            print("new ZipFile found at: " + fileToUnzip)
+            fileToUnzip = ZipFile(fileToUnzip)
 
     def checkForDirectories(self):
         with os.scandir(self.path) as listOfDirectories:
             for entry in listOfDirectories:
-                if not entry.is_file():
-                    self.directoriesToCheck.append(entry)
-        return self.directoriesToCheck
+                if not entry.is_file() and entry.name != '.git':
+                    entry = Folder(os.path.join(self.path, entry.name))
 
-
-
-
-
-
-fileTest = Folder('.', list(), list())
-fileDirectories = fileTest.checkForDirectories()
-fileZip = fileTest.checkForZippedFile()
-print("Directories: ")
-print(fileDirectories)
-print("Files to unzip: ")
-print(fileZip)
+fileTest = Folder('.')
+#fileDirectories = fileTest.checkForDirectories()
+#fileZip = fileTest.checkForZippedFile()
+#print("Directories: ")
+#print(fileDirectories)
+#print("Files to unzip: ")
+#print(fileZip)
 
 
 
